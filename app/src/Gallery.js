66af85daec45df635ac9210fd85c08dd7ce5e9e3
h8yourface <sejeks@gmail.com>
2018-11-19 10:49:50 +0400

    some fixes

--- app/src/Gallery.js
+++ app/src/Gallery.js
@@ -1,5 +1,6 @@
 import React from "react";
 import $ from "jquery";
+import './index.css';
 
 class Gallery extends React.Component {
     constructor(){
@@ -11,15 +12,17 @@ class Gallery extends React.Component {
     componentDidMount() {
         console.log("componentDidMount 1");
         $.ajax({
-            url: "https://api.vk.com/method/photos.getAll?owner_id=40519043&count=20&access_token=4c288afa12d1dfbdff6c4a8b6e2adc73cda49fefd0fe87643c8837ee0699deea90dcf5fd8717d12b643d7&v=5.52",
+            url: "https://api.vk.com/method/photos.getAll?owner_id=40519043&count=60&access_token=f5424ccc72c0f4b7497cc7b370050e2c131ef17a9aab98078afcc1377f3a6210528e3cf61fda0288bb991&v=5.52",
             method: 'GET',
             dataType: 'JSONP'
         }).done((data) => {
-            let urlsArray;
+            let urlsArray = [];
             const dataArray= data.response.items;
             for (let i = 0; i < dataArray.length; i++){
-                urlsArray[i] = dataArray[i].photo_130;
+                urlsArray[i] = dataArray[i].photo_604;
             }
+            console.log(urlsArray);
+
             this.setState({urls:urlsArray});
             console.log(this.state);
             console.log("componentDidMount 2");
@@ -28,8 +31,8 @@ class Gallery extends React.Component {
 
     renderImage(imageUrl) {
         return (
-            <div>
-                <img src={imageUrl} />
+            <div className="images">
+                <img src={imageUrl} key={imageUrl.index}/>
             </div>
         );
     }
@@ -37,8 +40,8 @@ class Gallery extends React.Component {
     get gallery() {
         return (
             <div className="gallery">
-                <div className="images">
-                    {this.props.imageUrls.map(imageUrl => this.renderImage(imageUrl))}
+                <div>
+                    {this.state.urls.map(imageUrl => this.renderImage(imageUrl))}
                 </div>
             </div>
         );
@@ -48,7 +51,7 @@ class Gallery extends React.Component {
         return this.state.urls.length ? this.gallery : null;
     }
 }
-Gallery.propTypes = {
-    imageUrls: React.PropTypes.arrayOf(React.PropTypes.string).isRequired
-};
+// Gallery.propTypes = {
+//     imageUrls: React.PropTypes.arrayOf(React.PropTypes.string).isRequired
+// };
 export default Gallery;
